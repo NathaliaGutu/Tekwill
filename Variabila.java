e4d947f4f9f4e0676d6611b984a92cfe9ddd8c62
NathaliaGutu <109095488+NathaliaGutu@users.noreply.github.com>
2022-07-22 15:40:00 +0300

    Add files via upload

---
+++ Variabila.java
@@ -0,0 +1,11 @@
+public class Variabila {
+    public static void main (String[] args) {
+        int x = 13;
+        if ((x%2==0)&&(x>10)){
+        System.out.println ("Numarul este par");
+        }
+    else if((x%2!=0)&&(x<15)){
+    System.out.println("Numarul este impar");
+    }
+}
+}
